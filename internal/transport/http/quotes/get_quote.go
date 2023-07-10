7ab56a51160108f24cfaefab8d5ca9b1f7dbfc2a
Daniel Orlov <daniel.orlov@protonmail.com>
2023-07-10 12:20:37 +0200

    feat: make quotes handler tests green

--- internal/transport/http/quotes/get_quote.go
+++ internal/transport/http/quotes/get_quote.go
@@ -1,11 +1,30 @@
 package quotes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	"github.com/daniel-orlov/quotes-server/internal/domain/model"
 )
 
 // GetQuote handles the request for getting a quote.
 func (h *Handler) GetQuote(c *gin.Context) {
-	// not implemented
-	c.AbortWithStatusJSON(501, nil)
+	// Call the service.
+	quote, err := h.service.GetRandomQuote(c.Request.Context())
+	// Handle the error.
+	if err != nil {
+		// Log the actual error.
+		h.logger.Error("failed to get quote", zap.Error(err))
+
+		// Return a generic error to the client.
+		c.AbortWithStatusJSON(http.StatusInternalServerError, model.Quote{})
+
+		// Exit the function.
+		return
+	}
+
+	// Return the quote to the client.
+	c.JSON(http.StatusOK, quote)
 }
